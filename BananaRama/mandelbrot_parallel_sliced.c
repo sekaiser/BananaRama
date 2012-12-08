2b262fa129035dc46b23789d1e2a1c6c739ac9b0
Home <home@ubuntu.ubuntu-domain>
2012-12-08 14:01:41 +0100

    fixes

--- BananaRama/mandelbrot_parallel_sliced.c
+++ BananaRama/mandelbrot_parallel_sliced.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include "mandelbrot.h"
+
 ////////////////////////////////////////////////////////////////////////
 //                                                                    //
 // The Mandelbrot Set                                                 //
@@ -100,71 +101,86 @@ int setSlices(int *iarrBegin, int *iarrEnd, int *iarrOffset) {
 void master(int numProcs, int *iarrBegin, int *iarrEnd, 
             int average, double crMin, double ciMin, double dcr, 
             double dci, double *storage, int *offset ) {
+
   int    k, k_slice, slice, process;
   int    number, source;
   static int slices[MAX_PROCESSES];
   int    kill, recv_slice;
   double mandelbrot[NX * NY];
   MPI_Status status;
+
   /* scan over slices */
   slice = 0;
+
   /* send a slice to each slave */
   for (process = 1; process < numProcs; process++) { 
-    printf("1st loop start\n");
     /* generate Mandelbrot set in each process */
     MPI_Ssend(&slice, 1, MPI_INT, process, 325, MPI_COMM_WORLD);
     slices[process] = slice;
     slice++;
-    printf("1st loop end\n");
   }
+
   /* scan over received slices */
   for (k_slice = 0; k_slice < N_SLICES; k_slice++) {
-    printf("2nd loop start on round %d\n", k_slice);
+
     /* receive a slice */
     number = NX * (average + 1);
-    printf("2nd loop recv\n"); 
 
     MPI_Recv(storage, number, MPI_DOUBLE, MPI_ANY_SOURCE, 327, MPI_COMM_WORLD, &status);
+
     /* source of slice */
     source = status.MPI_SOURCE;
+
     /* received slice */ 
     recv_slice = slices[source];
-    
-    printf("2nd loop recieved slice %d\n",recv_slice);
 
     /* send a slice to this slave */
-    if (slice < N_SLICES) { 
-      
-      printf("2nd loop sending new slice %d\n", slice);
+    if (slice < N_SLICES) {   
       MPI_Ssend(&slice, 1, MPI_INT, source, 325, MPI_COMM_WORLD);
       slices[source] = slice;
       slice++;
     } else {
       kill = -1;
       for (process = 1; process < numProcs; process++) {
-	printf("sending kill to WORLD\n");
         MPI_Ssend(&kill, 1, MPI_INT, process, 325, MPI_COMM_WORLD);
       }
       
-	fprintf(stderr, " The Mandelbrot Set will be written out \n");
-  	for (k = 0; k < NX * NY; k++) {
-  		printf(" %f \n", mandelbrot[k]);
-  	}
+      for (k = 0; k < NX * NY; k++) {
+        printf(" %f \n", mandelbrot[k]);
+      }
 
+      /* writes the data to a TGA file */
+      if ((fd = open("mand.tga", O_RDWR+O_CREAT, 0)) == -1)
+      {
+        printf("error opening file\n");
+        exit(1);
+      }
+    
+      buffer[0] = 0;
+      buffer[1] = 0;
+      buffer[2] = 2;
+      buffer[8] = 0; buffer[9] = 0;
+      buffer[10] = 0; buffer[11] = 0;
+      buffer[12] = (NX & 0x00FF); buffer[13] = (NX & 0xFF00) >> 8;
+      buffer[14] = (NY & 0x00FF); buffer[15] = (NY & 0xFF00) >> 8;
+      buffer[16] = 24;
+      buffer[17] = 0;
+      write(fd, buffer, 18);
+      write(fd, mandelbrot, NX*NY);
+      close(fd);
 
       free(storage);
       MPI_Finalize();
       exit(0);
-
     }
     
     /* actual number */
     number = NX * (iarrEnd[recv_slice] - iarrBegin[recv_slice] + 1);
+
     /* store set in matrix */
     for (k=0 ; k < number; k++ ) {
       mandelbrot[offset[recv_slice] + k] = storage[k];
     }
-    printf("2nd loop end\n");
   }
 }
                                             
@@ -172,10 +188,12 @@ double iterate(double cReal, double cImg, int *count) {
   double zReal, zImg, zCurrentReal, zMagnitude;
   double color;
   int    counter;
+  
   /* z = 0 */
   zReal = 0.0;
   zImg  = 0.0;
   counter = 0;
+  
   while (counter < MAX_ITERATIONS) {
     zCurrentReal = zReal;
     zReal = zReal*zReal - zImg * zImg + cReal;
@@ -186,6 +204,7 @@ double iterate(double cReal, double cImg, int *count) {
       break;
     }
   }
+  
   //#ifdef test
   //if (zMagnitude < THRESHOLD_RADIUS) { 
   //  printf (" %f %f  \n ", cReal, cImg );
@@ -193,8 +212,10 @@ double iterate(double cReal, double cImg, int *count) {
   //#endif
   count++;
   color = (double)(255*counter) / (double)MAX_ITERATIONS;
+
   return color;
 }
+
 /* Calculate the Mandelbrot set in slice  */
 void computeSlice(int slice, int *iarrBegin, int *iarrEnd,
                      int *count, double crMin, double ciMin,
@@ -212,6 +233,7 @@ void computeSlice(int slice, int *iarrBegin, int *iarrEnd,
     }
   }
 }
+
 void slave(int rank, int *iarrBegin, int *iarrEnd,
            int average, double crMin, double ciMin, double dcr, 
            double dci, double *storage) {
@@ -225,17 +247,22 @@ void slave(int rank, int *iarrBegin, int *iarrEnd,
     /* a new slice to calculate */
     MPI_Recv(&slice, 1, MPI_INT,0, 325, MPI_COMM_WORLD, &status);
     printf("slave %d will process slice: %d\n",rank, slice);
+
     /* suicide signal */
     if (slice < 0) {
       printf("Process %d exiting work loop.\n", rank);
       MPI_Finalize();
       exit(0);
     }
+
     /* calculate requested slice */
     computeSlice(slice, iarrBegin, iarrEnd, &count, crMin, ciMin, dcr, dci, storage);
+
     /* send results back to master */
     number = NX * (average + 1);
+
     MPI_Ssend(storage, number, MPI_DOUBLE, 0, 327, MPI_COMM_WORLD);
+
     //#ifdef test
     //fprintf( stderr, "slave %d  -->  slice %d is calculated & sent\n", rank, slice );
     //#endif
@@ -257,6 +284,7 @@ int main(int argc, char *argv[]) {
  
   /* memory allocation for color storage */
   storage = (double *)malloc(NX * (width_avg_slice + 1) * sizeof(double));
+
   /* master node */
   if (rank == 0) {
     master(numProcs, iarrBeginSlice, iarrEndSlice, width_avg_slice,
--- BananaRama/mandelbrot_seq.c
+++ BananaRama/mandelbrot_seq.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 
-#define MAX_ITERATIONS 60
-#define NX 600                 
-#define NY 500
+#define MAX_ITERATIONS 120
+#define NX 640                 
+#define NY 480
 
 
 struct BMPHeader
@@ -29,119 +30,6 @@ struct BMPHeader
                              are important */
 };
 
-
-
-int assemble_work(char *work_result)
-{
-    // Allocate memory for resulting image
-    int bitmap_pixel_count = NX * NY;
-    char *bitmap = malloc(bitmap_pixel_count * 3 * sizeof(char));
-    if (bitmap == NULL)
-        return printf("malloc\n");
-    
-    // Set pixel colors according to computed limit hit
-    int count;
-    for (count = 0; count < NX * NY; count++)
-    {
-        char color = work_result[count] == 0 ? 0 : 255;
-        bitmap[count * 3] = color;
-        bitmap[count * 3 + 1] = color;
-        bitmap[count * 3 + 2] = color;
-    }
-    
-    printf("hier\n");
-    
-    // Write image into bitmap file
-    int exit_code = EXIT_SUCCESS;
-    int result = write_bmp("filename.bmp", NX, NY, bitmap);
-    if (result != 1)
-        exit_code = error("write_bmp", result);
-    
-    free(bitmap);
-    printf("Image creation finished\n");
-    
-    return(exit_code);
-}
-
-int read_bmp(const char *filename, int *width, int *height, unsigned char *rgb)
-{
-    printf("Sorry, reading of .bmp files isn't supported yet.\n");
-    return(0);
-}
-
-int write_bmp(const char *filename, int width, int height, char *rgb)
-{
-    int i, j, ipos;
-    int bytesPerLine;
-    unsigned char *line;
-
-    FILE *file;
-    struct BMPHeader bmph;
-
-    /* The length of each line must be a multiple of 4 bytes */
-
-    bytesPerLine = (3 * (width + 1) / 4) * 4;
-
-    strcpy(bmph.bfType, "BM");
-    bmph.bfOffBits = 54;
-    bmph.bfSize = bmph.bfOffBits + bytesPerLine * height;
-    bmph.bfReserved = 0;
-    bmph.biSize = 40;
-    bmph.biWidth = width;
-    bmph.biHeight = height;
-    bmph.biPlanes = 1;
-    bmph.biBitCount = 24;
-    bmph.biCompression = 0;
-    bmph.biSizeImage = bytesPerLine * height;
-    bmph.biXPelsPerMeter = 0;
-    bmph.biYPelsPerMeter = 0;
-    bmph.biClrUsed = 0;       
-    bmph.biClrImportant = 0; 
-
-    file = fopen (filename, "wb");
-    if (file == NULL) return(0);
-  
-    fwrite(&bmph.bfType, 2, 1, file);
-    fwrite(&bmph.bfSize, 4, 1, file);
-    fwrite(&bmph.bfReserved, 4, 1, file);
-    fwrite(&bmph.bfOffBits, 4, 1, file);
-    fwrite(&bmph.biSize, 4, 1, file);
-    fwrite(&bmph.biWidth, 4, 1, file);
-    fwrite(&bmph.biHeight, 4, 1, file);
-    fwrite(&bmph.biPlanes, 2, 1, file);
-    fwrite(&bmph.biBitCount, 2, 1, file);
-    fwrite(&bmph.biCompression, 4, 1, file);
-    fwrite(&bmph.biSizeImage, 4, 1, file);
-    fwrite(&bmph.biXPelsPerMeter, 4, 1, file);
-    fwrite(&bmph.biYPelsPerMeter, 4, 1, file);
-    fwrite(&bmph.biClrUsed, 4, 1, file);
-    fwrite(&bmph.biClrImportant, 4, 1, file);
-  
-    line = malloc(bytesPerLine);
-    if (line == NULL)
-    {
-        printf("Can't allocate memory for BMP file.\n");
-        return(0);
-    }
-
-    for (i = height - 1; i >= 0; i--)
-    {
-        for (j = 0; j < width; j++)
-        {
-            ipos = 3 * (width * i + j);
-            line[3*j] = rgb[ipos + 2];
-            line[3*j+1] = rgb[ipos + 1];
-            line[3*j+2] = rgb[ipos];
-        }
-        fwrite(line, bytesPerLine, 1, file);
-    }
-
-    free(line);
-    fclose(file);
-
-    return(1);
-}
-
 int main(int argc, char **argv)
 {
     clock_t tstart = clock();
@@ -150,11 +38,15 @@ int main(int argc, char **argv)
     double c_real, c_img, crmin, crmax, cimin, cimax, 
           dcr, dci, z_current_real;
 
+    int inset;
     int i, j;
-    int    counter;
+    int counter;
+    double colour;
     int count;
-    char *rgb = malloc(3*NX*NY*sizeof(char));
-
+    char rgb[NX][NY][3];
+    int fd;
+    char buffer[100];
+    
     crmin=-1.7;
     crmax = 0.8;
     cimin = -1.0;
@@ -163,47 +55,74 @@ int main(int argc, char **argv)
     dcr = (crmax - crmin)/(NX-1);
     dci = (cimax - cimin)/(NY-1);
 
-    count = 0;
-    
-
     for ( i=0 ; i<NX; i++)
     {
 
       for ( j=0 ; j<NY; j++)
       {
 
-         c_real = crmin + i*dcr;
-         c_img = cimin + j*dci;
+        c_real = crmin + i*dcr;
+        c_img = cimin + j*dci;
+        
+        z_real = 0.0;
+        z_img  = 0.0;
+        
+        counter = 0;                        
+        inset = 1;
+        while (counter < MAX_ITERATIONS) 
+        {
+          z_current_real = z_real;
+          z_real = z_real*z_real - z_img*z_img + c_real;
+          z_img  = 2.0*z_current_real*z_img + c_img;
+          counter = counter + 1;
 
-         z_real = 0.0;
-         z_img  = 0.0;
+          z_magnitude = z_real*z_real+z_img*z_img;      
+          if (z_magnitude > 4) 
+          {
+            inset = 0;
+            colour = counter;
+            count = MAX_ITERATIONS;
+          }
+        }
+        
+        if (inset)
+        {
+            rgb[i][j][0] = 0;
+            rgb[i][j][1] = 0;
+            rgb[i][j][2] = 0;
+        }
+        else
+        { 
+            rgb[i][j][0] = colour / MAX_ITERATIONS * 255;
+            rgb[i][j][1] = colour / MAX_ITERATIONS * 255 / 2;
+            rgb[i][j][2] = colour / MAX_ITERATIONS * 255 / 2;
+        }    
+      }
+    }
 
-         counter = 0;
-                                     
-
-         while ( counter < MAX_ITERATIONS ) 
-         {
-            z_current_real = z_real;
-            z_real = z_real*z_real - z_img*z_img + c_real;
-            z_img  = 2.0*z_current_real*z_img + c_img;
-            counter = counter + 1;
-
-
-            z_magnitude = z_real*z_real+z_img*z_img;
-            if ( z_magnitude > 4 ) 
-  		{
-				rgb[i*j] = counter;
-				break;
-			}
-				count++;
-			}
-
-		}
-	}
-	
-	int dummy = assemble_work(rgb);
-	clock_t tend = clock();
-	double cpu_time = ((float)(tend-tstart)/CLOCKS_PER_SEC);
-
-	printf ( "finished after %f \n", cpu_time);
+    /* writes the data to a TGA file */
+    if ((fd = open("mand.tga", O_RDWR+O_CREAT, 0)) == -1)
+    {
+      printf("error opening file\n");
+      exit(1);
+    }
+    
+    buffer[0] = 0;
+    buffer[1] = 0;
+    buffer[2] = 2;
+    buffer[8] = 0; buffer[9] = 0;
+    buffer[10] = 0; buffer[11] = 0;
+    buffer[12] = (NX & 0x00FF); buffer[13] = (NX & 0xFF00) >> 8;
+    buffer[14] = (NY & 0x00FF); buffer[15] = (NY & 0xFF00) >> 8;
+    buffer[16] = 24;
+    buffer[17] = 0;
+    write(fd, buffer, 18);
+    write(fd, rgb, NX*NY*3);
+    close(fd);
+  
+    //int dummy = assemble_work(rgb);
+    clock_t tend = clock();
+    double cpu_time = ((float)(tend-tstart)/CLOCKS_PER_SEC);
+    
+    printf ( "finished after %f \n", cpu_time);
 }
