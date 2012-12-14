20fd5d65d2b9a820be448a8aa56b3f7c135ea220
Eric Kuhnt <kuhnt@uni-potsdam.de>
2012-12-14 13:46:19 +0100

    fixing master definition & identions

--- BananaRama/mandelbrot.h
+++ BananaRama/mandelbrot.h
@@ -1,66 +1,68 @@
 /*
- *
- * mandelbrot.h                                                       
- *                                                                   
- * Disclaimer:                                                     
- * This implementation of Mandelbrot is inspired by an excellent      
- * tutorial given by Michel Valleries, Professor for Physics at Drexel
- * University. You can find his tutorial here:                        
- * http://www.physics.drexel.edu/~valliere/PHYS405/Content.html      
- *                                                                   
- *                                                                   
- * Inspired by Prof. Velleries we programmed a parallel version     
- * Mandelbrot. Therefore, we used the Master-Slave model. In order 
- * to compute the area, the master process divides  the computation 
- * area into slices. These slices will be distributed among the     
- * available slaves, who perform the compuation. Afterwards the slaves
- * send the results back to the master process, who is will compute  
- * the received data in order to generate a picture.                  
- * The user interaction is realized by implementing a small dialog.  
- * Furthermore we tried to implement a solution, that is as fast as  
- * possible.                                                         
- *                                                                   
- *                                                                                                                                     
- *  author Sebastian Kaiser (743121)                                
- *  author Eric Kuhnt                                               
- *  author Robert                                                   
- *                                                                  
- *  version 1.0  12/06/12                                           
- *                                                                   
+  mandelbrot.h                                                       
+                                                                    
+  Disclaimer:                                                     
+  This implementation of Mandelbrot is inspired by an excellent      
+  tutorial given by Michel Valleries, Professor for Physics at Drexel
+  University. You can find his tutorial here:                        
+  http://www.physics.drexel.edu/~valliere/PHYS405/Content.html      
+                                                                    
+                                                                    
+  Inspired by Prof. Velleries we programmed a parallel version     
+  Mandelbrot. Therefore, we used the Master-Slave model. In order 
+  to compute the area, the master process divides  the computation 
+  area into slices. These slices will be distributed among the     
+  available slaves, who perform the compuation. Afterwards the slaves
+  send the results back to the master process, who is will compute  
+  the received data in order to generate a picture.                  
+  The user interaction is realized by implementing a small dialog.  
+  Furthermore we tried to implement a solution, that is as fast as  
+  possible.                                                         
+                                                                    
+                                                                                                                                      
+   author Sebastian Kaiser (743121)                                
+   author Eric Kuhnt                                               
+   author Robert                                                   
+                                                                   
+   version 1.0  12/06/12                                                                                                              
  */
 
 #ifndef MANDELBROT__H
 #define MANDELBROT__H
 
 /*
- * Constants
+   Constants
  */
 
 /* if |z| goes beyond the threshold, point C is not in the set */
 #define THRESHOLD_RADIUS 4.0
 
-/* Maximum number of iterations before declaring a point in the
- * Mandelbrot set */
+/* 
+   Maximum number of iterations before declaring a point in the
+   Mandelbrot set 
+ */
 #define MAX_ITERATIONS 120
 
 /* image size. default is 700x500 pixels */
 #define NX 640
 #define NY 400
 
-/* number of slices that are going to be distributed among the slave
- * processes. */
+/* 
+   number of slices that are going to be distributed among the slave
+   processes. 
+ */
 #define N_SLICES 64
 
 /* Maximum nuber of processes */
 #define MAX_PROCESSES 4
 
-/* #define debug */
+#define debug
 
 /*
- * Functions
+  Functions
  */
 
-/**
+/*
    Setup the grid in a complex C plane. Therefore, define the grid
    spanning of the complex plane for the C variable. In addition,
    define the C spacing in the complex plane (dcr, dci).
@@ -76,7 +78,7 @@ void setGrid(double *crMin, double *crMax, double *ciMin,
              double *ciMax, double *dcr, double *dci);
 
 
-/**
+/*
    Setup the slices through which compute the Mandelbrot set.
    
    param iarrBegin  Begin of slice.
@@ -87,7 +89,7 @@ void setGrid(double *crMin, double *crMax, double *ciMin,
 int setSlices(int *iarrBegin, int *iarrEnd, int *iarrOffset);
 
 
-/**
+/*
    Master node -- control the calculation.
 
    param numProcs  Number of processes.
@@ -101,11 +103,11 @@ int setSlices(int *iarrBegin, int *iarrEnd, int *iarrOffset);
    param dcr
    param dci
    param storage   Allocated memory for a slice.
-*/
+ */
 void master(int mpiSize, int *iarrBegin, int *iarrEnd,
             int average, double *storage, int *offset );
 
-/**
+/*
    Iterate the Mandelbrot map. Within each computation step, we
    need to check whether the given input point in complex C plane
    belongs to the set or not. 
@@ -118,7 +120,7 @@ void master(int mpiSize, int *iarrBegin, int *iarrEnd,
 double iterate(double cReal, double cImg);
 
 
-/**
+/*
    Calculate the Mandelbrot set in slice.
 
    param slice     Number of current slide.
@@ -136,7 +138,7 @@ void computeSlice(int slice, int *iarrBegin, int *iarrEnd,
                   double dcr, double dci, double *storage);
 
 
-/**
+/*
    Slave processes. They calculate a specific slice as
    received from process 0.
    
@@ -157,7 +159,7 @@ void slave(int rank, int *iarrBegin, int *iarrEnd, int average, double crMin, do
 void slave(int *iarrBegin, int *iarrEnd, int average, double crMin, double ciMin, double dcr, double dci, double* storage);
 #endif
 
-/**
+/*
    Kill the slave processes.
    param mpiSize
  */
--- BananaRama/mandelbrot_parallel_sliced.c
+++ BananaRama/mandelbrot_parallel_sliced.c
@@ -4,53 +4,52 @@
 #include <mpi.h>
 #include <fcntl.h>
 #include "mandelbrot.h"
-/*
- *                                                                    
- * The Mandelbrot Set                                                
- *                                                                    
- * Computation: z_{i+1} = z_{i} * z_{i} + c                           
- *                                                                    
- * The Mandelbrot Set results from a very simple map in the complex   
- * plane by following some rules:                                     
- *   1) For a given complex number c, start with z = 0, and iterate   
- *      the map above.                                                
- *                                                                    
- *   2) If z remains finite, even after an infinite number of         
- *      iterations, c belongs to M.                                   
- *                                                                    
- *   3) Repeat the procedure, or scan, for all c in the complex plane,
- *      to find the points belonging to M,                            
- *                                                                    
- *                                                                    
- * Disclaimer:                                                        
- * This implementation of Mandelbrot is inspired by an excellent      
- * tutorial given by Michel Valleries, Professor for Physics at Drexel
- * University. You can find his tutorial here:                        
- * http://www.physics.drexel.edu/~valliere/PHYS405/Content.html       
- *                                                                    
- *                                                                    
- * Inspired by Prof. Velleries we programmed a parallel version       
- * Mandelbrot. Therefore, we used the Master-Slave model. In order    
- * to compute the area, the master process divides  the computation   
- * area into slices. These slices will be distributed among the       
- * available slaves, who perform the compuation. Afterwards the slaves
- * send the results back to the master process, who is will compute   
- * the received data in order to generate a picture.                  
- * The user interaction is realized by implementing a small dialog.   
- * Furthermore we tried to implement a solution, that is as fast as   
- * possible.                                                          
- *                                                                    
- *                                                                    
- *                                                                    
- *  author Sebastian Kaiser (743121)                                 
- *  author Eric Kuhnt                                                
- *  author Robert                                                   
- *                                                                    
- *  version 1.0  12/06/12                                            
- *
+/*                                                                    
+  The Mandelbrot Set                                                
+                                                                     
+  Computation: z_{i+1} = z_{i} * z_{i} + c                           
+                                                                     
+  The Mandelbrot Set results from a very simple map in the complex   
+  plane by following some rules:                                     
+    1) For a given complex number c, start with z = 0, and iterate   
+       the map above.                                                
+                                                                     
+    2) If z remains finite, even after an infinite number of         
+       iterations, c belongs to M.                                   
+                                                                     
+    3) Repeat the procedure, or scan, for all c in the complex plane,
+       to find the points belonging to M,                            
+                                                                     
+                                                                     
+  Disclaimer:                                                        
+  This implementation of Mandelbrot is inspired by an excellent      
+  tutorial given by Michel Valleries, Professor for Physics at Drexel
+  University. You can find his tutorial here:                        
+  http://www.physics.drexel.edu/~valliere/PHYS405/Content.html       
+                                                                     
+                                                                     
+  Inspired by Prof. Velleries we programmed a parallel version       
+  Mandelbrot. Therefore, we used the Master-Slave model. In order    
+  to compute the area, the master process divides  the computation   
+  area into slices. These slices will be distributed among the       
+  available slaves, who perform the compuation. Afterwards the slaves
+  send the results back to the master process, who is will compute   
+  the received data in order to generate a picture.                  
+  The user interaction is realized by implementing a small dialog.   
+  Furthermore we tried to implement a solution, that is as fast as   
+  possible.                                                          
+                                                                     
+                                                                     
+                                                                     
+   author Sebastian Kaiser (743121)                                 
+   author Eric Kuhnt                                                
+   author Robert                                                   
+                                                                     
+   version 1.0  12/06/12                                            
+ 
  */
 
-/* Function implementations*/
+/* Function implementations */
 
 /* 
   Set up the grid in complex C plane to generate image.
@@ -58,40 +57,40 @@
 */
 void setGrid(double *crMin, double *crMax, double *ciMin,
              double *ciMax, double *dcr, double *dci) {
-  /* grid spanning for variable C */
-  *crMin = -1.7;
-  *crMax = 0.8;
-  *ciMin = -1.1;
-  *ciMax = 1.1;
-  /* C spacing */
-  *dcr = (*crMax - *crMin)/(NX - 1);
-  *dci = (*ciMax - *ciMin)/(NY - 1);
+	/* grid spanning for variable C */
+	*crMin = -1.7;
+	*crMax = 0.8;
+	*ciMin = -1.1;
+	*ciMax = 1.1;
+	/* C spacing */
+	*dcr = (*crMax - *crMin)/(NX - 1);
+	*dci = (*ciMax - *ciMin)/(NY - 1);
 }
 
 /* Setup the slices through which compute the Mandelbrot set  */
 int setSlices(int *iarrBegin, int *iarrEnd, int *iarrOffset) {
-  int slice, avg_width_slice, leftover, temp, tempOffset;
-  avg_width_slice = NY / N_SLICES;
-  leftover = NY % N_SLICES;
-  temp = -1;
-  tempOffset = 0;
+	int slice, avg_width_slice, leftover, temp, tempOffset;
+	avg_width_slice = NY / N_SLICES;
+	leftover = NY % N_SLICES;
+	temp = -1;
+	tempOffset = 0;
   
-  /* Initialize picture coordinates for slices. */
-  for (slice = 0; slice < N_SLICES; slice++) {
-    iarrBegin[slice] = temp + 1;
-    iarrEnd[slice] = iarrBegin[slice] + avg_width_slice - 1;
+	/* Initialize picture coordinates for slices. */
+	for (slice = 0; slice < N_SLICES; slice++) {
+		iarrBegin[slice] = temp + 1;
+		iarrEnd[slice] = iarrBegin[slice] + avg_width_slice - 1;
 
-    if (leftover>0) {
-      iarrEnd[slice]++;
-      leftover--;
-    }
+		if (leftover>0) {
+			iarrEnd[slice]++;
+			leftover--;
+		}
     
-    temp = iarrEnd[slice];
-    iarrOffset[slice] = tempOffset;
-    tempOffset = tempOffset + NX * (iarrEnd[slice] - iarrBegin[slice] + 1);
-  }
+		temp = iarrEnd[slice];
+		iarrOffset[slice] = tempOffset;
+		tempOffset = tempOffset + NX * (iarrEnd[slice] - iarrBegin[slice] + 1);
+	}
   
-  return avg_width_slice;
+	return avg_width_slice;
 }
 
 /* Master node -- control the calculation */
@@ -112,8 +111,8 @@ void master(int mpiSize, int *iarrBegin, int *iarrEnd,
 	#endif
   
   	/* 
-	 * initialize
- 	 * send a slice to each slave
+	  initialize
+ 	  send a slice to each slave
 	 */
   	for (process = 1; process < mpiSize; process++) { 
 
@@ -127,8 +126,8 @@ void master(int mpiSize, int *iarrBegin, int *iarrEnd,
 	}
   
 	/* 
-	 * processing
-	 * scan over received slices
+	  processing
+	  scan over received slices
 	 */
 	for (int k_slice = 0; k_slice < N_SLICES; k_slice++) {
 
@@ -224,48 +223,47 @@ void closeMPI(int mpiSize) {
 }
                                             
 double iterate(double cReal, double cImg) {
-  double zReal, zImg, zCurrentReal, zMagnitude;
-  double color;
-  int    counter;
+	double zReal, zImg, zCurrentReal, zMagnitude;
+	double color;
+	int    counter;
   
-  /* z = 0 */
-  zReal = 0.0;
-  zImg  = 0.0;
-  counter = 0;
-  while (counter < MAX_ITERATIONS) {
-    zCurrentReal = zReal;
-    zReal = zReal*zReal - zImg * zImg + cReal;
-    zImg  = 2.0 * zCurrentReal * zImg + cImg;
-    counter++;
-    zMagnitude = zReal * zReal + zImg * zImg;
+	/* z = 0 */
+	zReal = 0.0;
+	zImg  = 0.0;
+	counter = 0;
+	while (counter < MAX_ITERATIONS) {
+		zCurrentReal = zReal;
+		zReal = zReal*zReal - zImg * zImg + cReal;
+		zImg  = 2.0 * zCurrentReal * zImg + cImg;
+		counter++;
+		
+		zMagnitude = zReal * zReal + zImg * zImg;
+		if (zMagnitude > THRESHOLD_RADIUS) {
+			break;
+		}
+	}
 
-    if (zMagnitude > THRESHOLD_RADIUS) {
-      break;
-    }
-  }
+	color = (double)(255*counter) / (double)MAX_ITERATIONS;
 
-  color = (double)(255*counter) / (double)MAX_ITERATIONS;
-
-  return color;
+	return color;
 }
 
 /* Calculate the Mandelbrot set in slice  */
 void computeSlice(int slice, int *iarrBegin, int *iarrEnd,
                      double crMin, double ciMin,
                      double dcr, double dci,  double *storage) {
-  int i,j,k;
-  double cReal, cImg;
-  k = 0;
-  /* scan over slice */
-  for (j = iarrBegin[slice] ; j <= iarrEnd[slice]; j++) {
-    for (i = 0; i < NX; i++) {
-       cReal = crMin + i * dcr;
-       cImg = ciMin + j * dci;
-
-       /* Store resulting color value */
-       storage[k++] = iterate(cReal, cImg);
-    }
-  }
+	int i,j,k;
+	double cReal, cImg;
+	k = 0;
+	/* scan over slice */
+	for (j = iarrBegin[slice] ; j <= iarrEnd[slice]; j++) {
+		for (i = 0; i < NX; i++) {
+			cReal = crMin + i * dcr;
+			cImg = ciMin + j * dci;
+       			/* Store resulting color value */
+			storage[k++] = iterate(cReal, cImg);
+		}
+	}
 }
 
 #ifdef debug
@@ -276,73 +274,73 @@ void slave(int rank, int *iarrBegin, int *iarrEnd, int average,
 void slave(int *iarrBegin, int *iarrEnd, int average, double crMin,
 	   double ciMin, double dcr, double dci, double* storage) {
 #endif
-  int        number, slice;
-  MPI_Status status;
+	int        number, slice;
+	MPI_Status status;
 
-  for (;;) {
-    /* a new slice to calculate */
-    MPI_Recv(&slice, 1, MPI_INT,0, 325, MPI_COMM_WORLD, &status);
-	#ifdef debug
-    		printf("Slave '%d' will process slice: '%d'.\n", rank, slice);
-	#endif
+	for (;;) {
+		/* a new slice to calculate */
+		MPI_Recv(&slice, 1, MPI_INT,0, 325, MPI_COMM_WORLD, &status);
+		#ifdef debug
+    			printf("Slave '%d' will process slice: '%d'.\n", rank, slice);
+		#endif
 
-    /* suicide signal */
-    if (slice < 0) {
-	#ifdef debug
-      		printf("Slave '%d' exiting work loop.\n", rank);
-	#endif
-      MPI_Finalize();
-      exit(0);
-    }
+		/* suicide signal */
+		if (slice < 0) {
+			#ifdef debug
+				printf("Slave '%d' exiting work loop.\n", rank);
+			#endif
+			MPI_Finalize();
+			exit(0);
+		}
 
-    /* calculate requested slice */
-    computeSlice(slice, iarrBegin, iarrEnd, crMin, ciMin, dcr, dci, storage);
+		/* calculate requested slice */
+		computeSlice(slice, iarrBegin, iarrEnd, crMin, ciMin, dcr, dci, storage);
 
-    /* send results back to master */
-    number = NX * (average + 1);
-	#ifdef debug
-    		printf("Slave '%d' sends result for slice '%d' back to master.\n", rank, slice);
-	#endif
-    MPI_Ssend(storage, number, MPI_DOUBLE, 0, 327, MPI_COMM_WORLD);
-
-  }
+		/* send results back to master */
+		number = NX * (average + 1);
+		#ifdef debug
+    			printf("Slave '%d' sends result for slice '%d' back to master.\n", rank, slice);
+		#endif
+		MPI_Ssend(storage, number, MPI_DOUBLE, 0, 327, MPI_COMM_WORLD);
+	}
 }
 
 int main(int argc, char *argv[]) {
-  double crMin, crMax, ciMin, ciMax; 
-  double dcr, dci;
-  int    widthAvgSlice;
-  int    iarrBeginSlice[1000], iarrEndSlice[1000], offset[1000];
-  int    rank, mpiSize;
-  double *storage;
+	double crMin, crMax, ciMin, ciMax; 
+	double dcr, dci;
+	int    widthAvgSlice;
+	int    iarrBeginSlice[1000], iarrEndSlice[1000], offset[1000];
+	int    rank, mpiSize;
+	double *storage;
    
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   
-  widthAvgSlice = setSlices(iarrBeginSlice, iarrEndSlice, offset);
-  setGrid(&crMin, &crMax, &ciMin, &ciMax, &dcr, &dci);
+	widthAvgSlice = setSlices(iarrBeginSlice, iarrEndSlice, offset);
+	setGrid(&crMin, &crMax, &ciMin, &ciMax, &dcr, &dci);
  
-  /* memory allocation for color storage */
-  storage = (double *)malloc(NX * (widthAvgSlice + 1) * sizeof(double));
+	/* memory allocation for color storage */
+	storage = (double *)malloc(NX * (widthAvgSlice + 1) * sizeof(double));
 
-  /* master node */
-  if (rank == 0) {
-	#ifdef debug
-    		printf("Initialize master (pid: %d)\n",rank); 
-	#endif
-    master(mpiSize, iarrBeginSlice, iarrEndSlice, widthAvgSlice,
-           storage, offset);
-  }
-  /* slave nodes  */
-  else {
-	#ifdef debug
-    		printf("Initialize slave (pid: %d)\n",rank);
-		slave(rank, iarrBeginSlice, iarrEndSlice, widthAvgSlice, crMin, ciMin, dcr, dci, storage);
-	#else
-		slave(iarrBeginSlice, iarrEndSlice, widthAvgSlice, crMin, ciMin, dcr, dci, storage);
-	#endif
-  }	
-  
-  return 0;
+	/* master node */
+	if (rank == 0) {
+		#ifdef debug
+    			printf("Initialize master (pid: %d)\n",rank); 
+		#endif
+		master(mpiSize, iarrBeginSlice, iarrEndSlice, widthAvgSlice,
+		storage, offset);
+	} else {
+		/* slave nodes  */
+		#ifdef debug
+    			printf("Initialize slave (pid: %d)\n",rank);
+			slave(rank, iarrBeginSlice, iarrEndSlice, widthAvgSlice,
+				crMin, ciMin, dcr, dci, storage);
+		#else
+			slave(iarrBeginSlice, iarrEndSlice, widthAvgSlice, 
+				crMin, ciMin, dcr, dci, storage);
+		#endif
+	}
+
+  	return 0;
 }
