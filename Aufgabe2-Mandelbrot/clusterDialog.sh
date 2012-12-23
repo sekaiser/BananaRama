9b7b4c38c1debfa7070a82894e696f358e4697f7
Eric Kuhnt <kuhnt@uni-potsdam.de>
2012-12-23 01:46:59 +0000

    update

--- Aufgabe2-Mandelbrot/clusterDialog.sh
+++ Aufgabe2-Mandelbrot/clusterDialog.sh
@@ -4,6 +4,22 @@
 # by R. Fruth, S. Kaiser & E. Kuhnt
 #
 
+MB_WIDTH=""
+MB_HEIGHT=""
+MB_ITERATIONS=""
+MB_SLICES=""
+MB_BESTPIC=""
+MB_FILENAME=""
+MB_RED=""
+MB_GREEN=""
+MB_BLUE=""
+MB_REDBG=""
+MB_GREENBG=""
+MB_BLUEBG=""
+MB_REMAX=""
+MB_REMIN=""
+MB_IMMIN=""
+
 # checks if the binary is present
 function checkBinary {
 	# check if the binary exists
@@ -44,6 +60,30 @@ function main {
 	printDialogHeader
 
 	printDialogIntro
+
+	echo "Type the name of your job (default: mb): "
+	read PBS_NAME
+	if [ "$PBS_NAME" == "" ] ; then
+		PBS_NAME="#PBS -N mb"
+	else
+		PBS_NAME="#PBS -N $PBS_NAME"
+	fi
+
+	echo "Do you want to get notified via eMail if your job exits or aborts? Type your mail address: "
+	read PBS_MAIL
+	if [ ! "$PBS_MAIL" == "" ] ; then
+		PBS_MAIL="#PBS -m ae\n#PBS -M $PBS_MAIL"
+	fi
+	
+	PBS_MAILADDR=""		# user mail address
+	PBS_WALLTIME=""		# wall time on the cluster
+	PBS_NODES=""		# number of nodes
+	PBS_PPN=""		# cores per node
+
+	MPI_PATH="/cvos/shared/apps/mpich2/ge/gcc/64/1.3.1/bin/mpirun" # mpi path
+	MPI_NODES="-np 4"
+
+	echo -e "PBSmail: $PBS_MAIL"
 
 }
 
